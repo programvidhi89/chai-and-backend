a1e4497918c2fe32dac864136ed95720877453c2
Skuad <skuad@skuad-vidhis-Mac.local>
2024-02-08 17:00:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+  res.send('hello world')
+})
+
+app.get('/login',(req,res)=>{
+  res.send('<h1>please login at</h1>')
+})
+app.get('/youtube',(req,res)=>{
+  res.send('<h2>chai and coffie</h2>')
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
