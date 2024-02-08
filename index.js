51dd76e4db397fd7a75ef759fa1fcf728f8703be
programvidhi89 <vidhirajvanshi@gmail.com>
2024-02-08 17:26:21 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const port = 3000
-
+const user = {
+  name:"vidhi",
+  age:21
+}
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -15,6 +18,9 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
   res.send('<h2>chai and coffie</h2>')
+})
+app.get('/github',(req,res)=>{
+  res.json(user)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`)
